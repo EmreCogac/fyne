8fab005014e4c0cd1922aaa64205042eae55969f
EmreCogac <emremta9@gmail.com>
2024-03-18 03:33:15 +0300

    Beta Update

--- main.go
+++ main.go
@@ -3,49 +3,86 @@ package main
 import (
 	// "fmt"
 
+	"image/color"
 	"net"
 	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/skip2/go-qrcode"
+	"main.go/router"
 	// "fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	var box *fyne.Container
+	var port string = ":8080"
+	var qr *canvas.Image
+	r := router.SetUpRouter()
 	a := app.New()
+
 	w := a.NewWindow("deneme")
 	w.Resize(fyne.NewSquareSize(700))
-	//var box *fyne.Container
-	//label := widget.NewLabel("data")
-	// button := widget.NewButton("button name", func() {
-	// 	fmt.Println("deneme")
-	// })
-	// url, _ := url.Parse("https://www.linkedin.com/in/emre-aktas-9176a31a6/")
-	// hyperlink := widget.NewHyperlink("linkedin", url)
+	ip := getIp()
+	portEditText := widget.NewEntry()
+	portEditText.SetPlaceHolder("Hangi Portu kullanmak istersiniz ?")
+	color := color.NRGBA{R: 0, G: 255, B: 255, A: 255}
+	text := canvas.NewText(ip+port, color)
+	text.Alignment = fyne.TextAlign(fyne.TextAlignCenter)
+	generateQr(ip, port)
+	qr = canvas.NewImageFromFile("./qr.png")
+	qr.FillMode = canvas.ImageFillOriginal
+	button := widget.NewButton("Generate qr", func() {
+		port = ":" + portEditText.Text
+		generateQr(ip, port)
+		qr = canvas.NewImageFromFile("./qr.png")
+		qr.FillMode = canvas.ImageFillOriginal
+		text.Text = ip + port
+		text.Alignment = fyne.TextAlign(fyne.TextAlignCenter)
+		qr.Refresh()
+		text.Refresh()
+		box.Refresh()
+		go r.Run(port)
+
+	})
+	box = container.NewVBox(
+		text,
+		button,
+		qr,
+		portEditText,
+	)
+	w.SetContent(box)
+
+	w.SetFixedSize(true)
+
+	w.ShowAndRun()
+
+}
+
+func generateQr(ip string, port string) {
+	_ = qrcode.WriteFile(ip+port, qrcode.High, 500, "./qr.png")
+}
+
+func getIp() string {
 	host, _ := os.Hostname()
 	addrs, _ := net.LookupIP(host)
 	for _, addr := range addrs {
 		if ipv4 := addr.To4(); ipv4 != nil {
-			_ = qrcode.WriteFile(ipv4.String(), qrcode.High, 500, "./qr.png")
-			// color := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
-
-			// text := canvas.NewText(ipv4.String(), color)
-			// box = container.NewVBox(
-			// 	text,
-
-			//)
-
+			return ipv4.String()
 		}
-
 	}
-	images := canvas.NewImageFromFile("./qr.png")
-	w.SetContent(images)
-
-	w.SetFixedSize(true)
-	w.ShowAndRun()
+	return " "
 }
+
+//label := widget.NewLabel("data")
+// button := widget.NewButton("button name", func() {
+// 	fmt.Println("deneme")
+// })
+// url, _ := url.Parse("https://www.linkedin.com/in/emre-aktas-9176a31a6/")
+// hyperlink := widget.NewHyperlink("linkedin", url)
 
 // func getIP(color color.Color) (*canvas.Text, *canvas.Image) {
 // 	var textNull *canvas.Text = canvas.NewText("", color)
--- router/route.go
+++ router/route.go
@@ -1,1 +1,20 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+func SetUpRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	r.GET("/data/left", test)
+	r.GET("/data/right", test)
+	r.GET("/test", test)
+
+	return r
+
+}
+func test(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"json data": "json data",
+	})
+}
